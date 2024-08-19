b41949a108d1fba3b5ca7ca5472fe0ed25762f2b
Parth panchal <40067366+parthfloyd@users.noreply.github.com>
2024-08-19 06:30:23 -0700

    Integrate Splash activity (#35)
    * (feat) added splash screen to open required activity on app start.
    
    * - refactored loginActivity after merge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,7 +88,8 @@ publishing {
 
 
 dependencies {
-  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+  implementation("androidx.activity:activity:1.9.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
   implementation("androidx.activity:activity-ktx:1.8.2")
   implementation("androidx.appcompat:appcompat:1.6.1")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,8 @@ buildscript {
 
   plugins {
     id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
-}
+      id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+  }
 
 allprojects {
   repositories {
