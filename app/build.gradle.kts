8551492cd67c989ee85cec5bd0348261e8c2bef9
Fred Deniger <fdeniger@icrc.org>
2024-08-29 14:36:47 +0200

    test logs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,23 +23,23 @@ android {
 //    server
 
     //    server
-    setResValue("fhir_base_url", this)
-    setResValue("openmrs_rest_url", this)
-    setResValue("check_server_url", this)
+    setResValue(providers,"fhir_base_url", this)
+    setResValue(providers,"openmrs_rest_url", this)
+    setResValue(providers,"check_server_url", this)
     //    oauth
-    setResValue("auth_authorization_scope", this)
-    setResValue("auth_client_id", this)
-    setResValue("auth_redirect_uri_host", this)
-    setResValue("auth_redirect_uri_path", this)
-    setResValue("auth_discovery_uri", this)
-    setResValue("auth_end_session_endpoint", this)
-    setResValue("auth_authorization_endpoint_uri", this)
-    setResValue("auth_token_endpoint_uri", this)
-    setResValue("auth_user_info_endpoint_uri", this)
-    setResValue("auth_registration_endpoint_uri", this)
+    setResValue(providers,"auth_authorization_scope", this)
+    setResValue(providers,"auth_client_id", this)
+    setResValue(providers,"auth_redirect_uri_host", this)
+    setResValue(providers,"auth_redirect_uri_path", this)
+    setResValue(providers,"auth_discovery_uri", this)
+    setResValue(providers,"auth_end_session_endpoint", this)
+    setResValue(providers,"auth_authorization_endpoint_uri", this)
+    setResValue(providers,"auth_token_endpoint_uri", this)
+    setResValue(providers,"auth_user_info_endpoint_uri", this)
+    setResValue(providers,"auth_registration_endpoint_uri", this)
 
-    setResValue("auth_https_required", this, "bool")
-    setResValue("auth_replace_localhost_by_10_0_2_2", this, "bool")
+    setResValue(providers,"auth_https_required", this, "bool")
+    setResValue(providers,"auth_replace_localhost_by_10_0_2_2", this, "bool")
 
   }
   buildTypes {
@@ -81,15 +81,15 @@ android {
 
 
 dependencies {
-  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+  coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.1")
   implementation(project(":core"))
 }
 
 
 
 
-fun setResValue(propertyName: String, variants: VariantDimension, type: String = "string") {
-  val prop = gradleLocalProperties(rootDir).getProperty(propertyName)
+fun setResValue(providers: ProviderFactory, propertyName: String, variants: VariantDimension, type: String = "string") {
+  val prop = gradleLocalProperties(rootDir,providers).getProperty(propertyName)
   if (prop != null) {
     variants.resValue(type, propertyName, prop)
   }
