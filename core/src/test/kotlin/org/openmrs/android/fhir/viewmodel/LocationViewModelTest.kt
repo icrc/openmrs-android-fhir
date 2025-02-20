0ddb0ecd523e840cc5fedd817457eb268c50db07
Parth Panchal <parth6606@gmail.com>
2025-02-19 23:49:39 -0800

    commented out location test -to be added later

--- core/src/test/kotlin/org/openmrs/android/fhir/viewmodel/LocationViewModelTest.kt
+++ core/src/test/kotlin/org/openmrs/android/fhir/viewmodel/LocationViewModelTest.kt
@@ -28,78 +28,63 @@
 */
 package org.openmrs.android.fhir.viewmodel
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
-import com.google.android.fhir.FhirEngine
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.rules.TestRule
-import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
-import org.mockito.junit.MockitoJUnitRunner
-import org.openmrs.android.fhir.data.remote.ApiManager
+// @RunWith(MockitoJUnitRunner::class)
+// class LocationViewModelTest {
+//
+//  @get:Rule var rule: TestRule = InstantTaskExecutorRule()
+//
+//  private val testDispatcher = TestCoroutineDispatcher()
+//
+//  @Mock private lateinit var fhirEngine: FhirEngine
+//
+//  @Mock private lateinit var apiManager: ApiManager
+//
+//  private lateinit var locationViewModel: LocationViewModel
+//
+//  @Mock private lateinit var observer: Observer<List<LocationViewModel.LocationItem>>
 
-@RunWith(MockitoJUnitRunner::class)
-class LocationViewModelTest {
 
-  @get:Rule var rule: TestRule = InstantTaskExecutorRule()
+//  @Before
+//  fun setUp() {
+//    MockitoAnnotations.openMocks(this)
+//    Dispatchers.setMain(testDispatcher)
+//    locationViewModel = LocationViewModel(applicationContext, fhirEngine, apiManager)
+//    locationViewModel.locations.observeForever(observer)
+//  }
 
-  private val testDispatcher = TestCoroutineDispatcher()
 
-  @Mock private lateinit var fhirEngine: FhirEngine
+//  @ExperimentalCoroutinesApi
+//  @After
+//  fun tearDown() {
+//    Dispatchers.resetMain()
+//    testDispatcher.cleanupTestCoroutines()
+//  }
 
-  @Mock private lateinit var apiManager: ApiManager
 
-  private lateinit var locationViewModel: LocationViewModel
-
-  @Mock private lateinit var observer: Observer<List<LocationViewModel.LocationItem>>
-
-  @Before
-  fun setUp() {
-    MockitoAnnotations.openMocks(this)
-    Dispatchers.setMain(testDispatcher)
-    //    locationViewModel = LocationViewModel(applicationContext, fhirEngine, apiManager)
-    locationViewModel.locations.observeForever(observer)
-  }
-
-  @ExperimentalCoroutinesApi
-  @After
-  fun tearDown() {
-    Dispatchers.resetMain()
-    testDispatcher.cleanupTestCoroutines()
-  }
-
-  // TODO: update the test
-  //  @OptIn(ExperimentalCoroutinesApi::class)
-  //  @Test
-  //  fun testGetLocations() = runBlockingTest {
-  //    val location =
-  //      Location().apply {
-  //        id = "1"
-  //        name = "Location 1"
-  //        status = Location.LocationStatus.ACTIVE
-  //      }
-  //
-  //    val searchResult = mutableListOf(SearchResult(location, mapOf(), mapOf()).apply {})
-  //
-  //    Mockito.`when`(
-  //        fhirEngine.search<Location> { Mockito.any() },
-  //      )
-  //      .thenReturn(searchResult)
-  //
-  //    locationViewModel.getLocations(online = false)
-  //
-  //    testDispatcher.scheduler.advanceUntilIdle()
-  //
-  //    Mockito.verify(observer).onChanged(Mockito.anyList())
-  //    assert(locationViewModel.locations.value?.size == 1)
-  //    assert(locationViewModel.locations.value?.get(0)?.name == "Location 1")
-  //  }
-}
+// TODO: update the test
+//  @OptIn(ExperimentalCoroutinesApi::class)
+//  @Test
+//  fun testGetLocations() = runBlockingTest {
+//    val location =
+//      Location().apply {
+//        id = "1"
+//        name = "Location 1"
+//        status = Location.LocationStatus.ACTIVE
+//      }
+//
+//    val searchResult = mutableListOf(SearchResult(location, mapOf(), mapOf()).apply {})
+//
+//    Mockito.`when`(
+//        fhirEngine.search<Location> { Mockito.any() },
+//      )
+//      .thenReturn(searchResult)
+//
+//    locationViewModel.getLocations(online = false)
+//
+//    testDispatcher.scheduler.advanceUntilIdle()
+//
+//    Mockito.verify(observer).onChanged(Mockito.anyList())
+//    assert(locationViewModel.locations.value?.size == 1)
+//    assert(locationViewModel.locations.value?.get(0)?.name == "Location 1")
+//  }
+// }
