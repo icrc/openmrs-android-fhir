47ae9ea5025f50cba18a03d3714a6f02d8fdee44
Jose Francisco <josecarlos.francisco@merkle.com>
2025-05-13 17:31:32 +0100

    (bump) navigation-safe-args-gradle-plugin to 2.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
     classpath("com.google.gms:google-services:4.4.2")
     classpath("com.diffplug.spotless:spotless-plugin-gradle:6.21.0")
-    classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.3")
+    classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.9.0")
 
     // NOTE: Do not place your application dependencies here; they belong
     // in the individual module build.gradle.kts files
