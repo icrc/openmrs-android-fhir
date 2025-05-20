87256fb2ec68fc827ed0fd6d86c875c7fc87a400
Parth Panchal <parth6606@gmail.com>
2025-05-20 01:41:51 -0700

    minor nits on submit for register patient.

--- core/src/main/java/org/openmrs/android/fhir/fragments/AddPatientFragment.kt
+++ core/src/main/java/org/openmrs/android/fhir/fragments/AddPatientFragment.kt
@@ -102,6 +102,7 @@ class AddPatientFragment : Fragment(R.layout.add_patient_fragment) {
             PreferenceKeys.LOCATION_ID,
           )
     }
+    observeLoading()
     if (savedInstanceState == null) {
       viewModel.getEmbeddedQuestionnaire(
         getString(R.string.registration_questionnaire_name),
@@ -163,6 +164,7 @@ class AddPatientFragment : Fragment(R.layout.add_patient_fragment) {
   }
 
   private fun onSubmitAction() {
+    viewModel.isLoading.value = true
     lifecycleScope.launch {
       val questionnaireFragment =
         childFragmentManager.findFragmentByTag(QUESTIONNAIRE_FRAGMENT_TAG) as QuestionnaireFragment
@@ -176,11 +178,14 @@ class AddPatientFragment : Fragment(R.layout.add_patient_fragment) {
 
   private fun observePatientSaveAction() {
     viewModel.isPatientSaved.observe(viewLifecycleOwner) {
+      viewModel.isLoading.value = false
       if (!it) {
-        Toast.makeText(requireContext(), "Inputs are missing.", Toast.LENGTH_SHORT).show()
+        Toast.makeText(requireContext(), getString(R.string.inputs_are_missing), Toast.LENGTH_SHORT)
+          .show()
         return@observe
       }
-      Toast.makeText(requireContext(), "Patient is saved.", Toast.LENGTH_SHORT).show()
+      Toast.makeText(requireContext(), getString(R.string.patient_is_saved), Toast.LENGTH_SHORT)
+        .show()
       NavHostFragment.findNavController(this).navigateUp()
     }
   }
@@ -188,6 +193,18 @@ class AddPatientFragment : Fragment(R.layout.add_patient_fragment) {
   private fun observeQuestionnaire() {
     viewModel.embeddedQuestionnaire.observe(viewLifecycleOwner) {
       it?.let { addQuestionnaireFragment(it) }
+    }
+  }
+
+  private fun observeLoading() {
+    viewModel.isLoading.observe(viewLifecycleOwner) {
+      if (it) {
+        binding.progressBar.visibility = View.VISIBLE
+        binding.touchBlockerView.visibility = View.VISIBLE // Show the blocker
+      } else {
+        binding.progressBar.visibility = View.GONE
+        binding.touchBlockerView.visibility = View.GONE // Hide the blocker
+      }
     }
   }
 
